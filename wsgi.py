0db92e5d2aa356a94e9b6ed87a3cf24f7cc0e3a2
getson7070 <getson.ind@gmail.com>
2025-09-29 17:22:38 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 # wsgi.py — very first lines
 import eventlet
 eventlet.monkey_patch(all=True)
-
+# app/__init__.py
+from flask_socketio import SocketIO
+socketio = SocketIO(async_mode="eventlet")  # **no** message_queue unless configured
 import os
 from app import create_app  # or however your factory is named
 app = create_app()
