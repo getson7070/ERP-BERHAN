607344c7092dc8037387d515bfa7d8232953868d
getson7070 <getson.ind@gmail.com>
2025-09-30 22:38:03 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,22 +1,15 @@
 # gunicorn.conf.py
-import multiprocessing
-import os
+import multiprocessing, os
 
 bind = "0.0.0.0:" + os.getenv("PORT", "10000")
-workers = int(os.getenv("WEB_CONCURRENCY", str(multiprocessing.cpu_count()))) or 2
-
-# Use threads instead of eventlet/gevent for Python 3.13 compatibility.
+workers = int(os.getenv("WEB_CONCURRENCY", str(max(2, multiprocessing.cpu_count()))))
 worker_class = "gthread"
 threads = int(os.getenv("GTHREADS", "4"))
 
-# Give startup a little time while DB warms up
 timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
 graceful_timeout = int(os.getenv("GUNICORN_GRACEFUL_TIMEOUT", "120"))
-
-# Keepalive helps with Render routing
 keepalive = int(os.getenv("GUNICORN_KEEPALIVE", "5"))
 
-# Access/error logs
 accesslog = "-"
 errorlog = "-"
 loglevel = os.getenv("LOGLEVEL", "info")
