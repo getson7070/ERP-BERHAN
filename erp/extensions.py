a60cdd6e418570a7e30e058b1b239f2ad1795902
getson7070 <getson.ind@gmail.com>
2025-09-30 22:53:00 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,19 +1,4 @@
 # erp/extensions.py
 from flask_sqlalchemy import SQLAlchemy
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-from authlib.integrations.flask_client import OAuth
-from flask_jwt_extended import JWTManager
-from flask_caching import Cache
-from flask_compress import Compress
-from flask_wtf import CSRFProtect
-from flask_babel import Babel
 
 db = SQLAlchemy()
-limiter = Limiter(key_func=get_remote_address)  # storage URI set in app.config
-oauth = OAuth()
-jwt = JWTManager()
-cache = Cache()
-compress = Compress()
-csrf = CSRFProtect()
-babel = Babel()
