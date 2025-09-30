16828bd46c74a420696e030924dad63fedfbe179
getson7070 <getson.ind@gmail.com>
2025-09-30 16:12:01 +0300

    Update __init__.py

--- erp/__init__.py
+++ erp/__init__.py
@@ -1,1 +1,3 @@
-from .app import create_app, socketio  # single source of truth
+# erp/__init__.py
+from .extensions import db, limiter, oauth, jwt, cache, compress, csrf, babel  # noqa: F401
+from .app import create_app, socketio  # noqa: F401
