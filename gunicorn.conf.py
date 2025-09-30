8bfd6f33aab8ff854977efcde8222ebf181c68f1
getson7070 <getson.ind@gmail.com>
2025-09-30 19:43:57 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,8 @@
 # gunicorn.conf.py
-import multiprocessing
 import os
 
 bind = "0.0.0.0:" + os.getenv("PORT", "10000")
-
-# IMPORTANT: use gevent workers (not eventlet)
-worker_class = "gevent"
+worker_class = "eventlet"
 workers = int(os.getenv("WEB_CONCURRENCY", "1"))
 worker_connections = 1000
 timeout = 60
