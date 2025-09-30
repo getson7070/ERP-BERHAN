820dabf395a082eddaa562dc04e3efaf4c1410c5
getson7070 <getson.ind@gmail.com>
2025-09-30 16:41:02 +0300

    Update __init__.py

--- erp/__init__.py
+++ erp/__init__.py
@@ -1,3 +1,4 @@
 # erp/__init__.py
 from .extensions import db, limiter, oauth, jwt, cache, compress, csrf, babel  # noqa: F401
 from .app import create_app, socketio  # noqa: F401
+from .constants import AUDIT_CHAIN_BROKEN  # noqa: F401
