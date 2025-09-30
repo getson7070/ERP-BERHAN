c26ae31f37fb39f3bd14ffd958339e2a7d0488e3
getson7070 <getson.ind@gmail.com>
2025-09-30 21:40:12 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,7 @@
-from erp.app import create_app
+# wsgi.py
+from erp.app import create_app, socketio
 
 app = create_app()
+
+# Gunicorn will import "wsgi:app". We do NOT call socketio.run() here.
+# Worker type is configured in gunicorn.conf.py.
