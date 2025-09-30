5ffb01410e82d783ba575cdfade815bb81d29526
getson7070 <getson.ind@gmail.com>
2025-09-30 16:10:52 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,5 +1,20 @@
+# erp/extensions.py
 from flask_sqlalchemy import SQLAlchemy
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from authlib.integrations.flask_client import OAuth
+from flask_jwt_extended import JWTManager
+from flask_caching import Cache
+from flask_compress import Compress
+from flask_wtf import CSRFProtect
+from flask_babel import Babel
 
-# Central place to instantiate extensions to avoid circular imports.
-# Additional extensions can be added here as the application grows.
+# Centralized singletons so routes can do: from erp import limiter, oauth, db, ...
 db = SQLAlchemy()
+limiter = Limiter(key_func=get_remote_address)  # storage configured via app.config
+oauth = OAuth()
+jwt = JWTManager()
+cache = Cache()
+compress = Compress()
+csrf = CSRFProtect()
+babel = Babel()
