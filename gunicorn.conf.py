a4b2b5e99cc590576987e4deb5af26a3fcb6eca2
getson7070 <getson.ind@gmail.com>
2025-09-30 19:38:50 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,20 +1,16 @@
 # gunicorn.conf.py
+import multiprocessing
 import os
-bind = f"0.0.0.0:{os.getenv('PORT', '10000')}"
+
+bind = "0.0.0.0:" + os.getenv("PORT", "10000")
+
+# IMPORTANT: use gevent workers (not eventlet)
+worker_class = "gevent"
 workers = int(os.getenv("WEB_CONCURRENCY", "1"))
-worker_class = "eventlet"
-threads = 1
 worker_connections = 1000
-timeout = 120
-graceful_timeout = 30
-keepalive = 2
-preload_app = False
+timeout = 60
+graceful_timeout = 60
+keepalive = 5
+loglevel = os.getenv("LOG_LEVEL", "info")
 accesslog = "-"
 errorlog = "-"
-loglevel = os.getenv("LOG_LEVEL", "info")
-secure_scheme_headers = {
-    "X-FORWARDED-PROTOCOL": "ssl",
-    "X-FORWARDED-PROTO": "https",
-    "X-FORWARDED-SSL": "on",
-}
-forwarded_allow_ips = "*"
