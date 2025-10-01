932742d3779454734eb44d0edb50cc934467751c
getson7070 <getson.ind@gmail.com>
2025-10-01 18:04:44 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-# wsgi.py
-import eventlet; eventlet.monkey_patch()
+# >>> this must be at the very top, before any other imports <<<
+import eventlet
+eventlet.monkey_patch()
+
 from erp.app import create_app
 app = create_app()
