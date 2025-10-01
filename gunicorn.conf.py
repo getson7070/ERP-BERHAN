1bc905ada41084d77894dad78bf57aa8501ad38c
getson7070 <getson.ind@gmail.com>
2025-10-01 14:57:35 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,15 +1,11 @@
 # gunicorn.conf.py
 import multiprocessing, os
-
 bind = "0.0.0.0:" + os.getenv("PORT", "10000")
-workers = int(os.getenv("WEB_CONCURRENCY", str(max(2, multiprocessing.cpu_count()))))
-worker_class = "gthread"
-threads = int(os.getenv("GTHREADS", "4"))
-
+workers = 1  # eventlet works with single worker
+worker_class = "eventlet"
 timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
 graceful_timeout = int(os.getenv("GUNICORN_GRACEFUL_TIMEOUT", "120"))
 keepalive = int(os.getenv("GUNICORN_KEEPALIVE", "5"))
-
 accesslog = "-"
 errorlog = "-"
 loglevel = os.getenv("LOGLEVEL", "info")
