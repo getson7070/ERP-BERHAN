81513463dba8d3e1b6ea3c2cee163f8c05409224
Your Name <you@example.com>
2025-10-02 14:21:18 +0300

    alembic: merge heads into single lineage

---
+++ migrations/versions/d1b125e62d70_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: d1b125e62d70
+Revises: e75b638f0ca3
+Create Date: 2025-10-02 14:21:17.315146
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd1b125e62d70'
+down_revision: Union[str, Sequence[str], None] = 'e75b638f0ca3'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
