018f29e38a4aee5bf782633ad4522a7f6b398603
getson7070 <getson.ind@gmail.com>
2025-10-02 09:53:25 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,19 +1,29 @@
 # gunicorn.conf.py
 import os
 
-# Render sets PORT; default to 10000 for local/run.
+# Tell Gunicorn where the app is (so you can run just: gunicorn --config gunicorn.conf.py)
+wsgi_app = "wsgi:app"
+
+# Render provides PORT; default for local is 10000
 bind = f"0.0.0.0:{os.getenv('PORT', '10000')}"
 
-# Eventlet worker for Socket.IO / long polling
+# Eventlet worker (Socket.IO / long-poll friendly)
 worker_class = "eventlet"
 workers = int(os.getenv("WEB_CONCURRENCY", "1"))
 
-# Timeouts appropriate for Eventlet
+# Make sure we do NOT preload the app (we want wsgi.py to run in worker import)
+preload_app = False
+
+# Reasonable timeouts for Eventlet
 timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
 graceful_timeout = int(os.getenv("GUNICORN_GRACEFUL_TIMEOUT", "120"))
 keepalive = int(os.getenv("GUNICORN_KEEPALIVE", "5"))
 
-# Logging to stdout/stderr
+# Logs to stdout/stderr (so Render captures them)
 accesslog = "-"
 errorlog = "-"
 loglevel = os.getenv("GUNICORN_LOGLEVEL", "info")
+
+# Avoid any stray threads with eventlet workers
+threads = 1
+
