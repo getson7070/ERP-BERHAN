5c06a195bd79badcaf2ad67ce3516d2e4cdceee5
getson7070 <getson.ind@gmail.com>
2025-10-04 14:06:33 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ eventlet.monkey_patch()
 from erp.app import create_app
 from erp.extensions import socketio
 
-app = create_app(
+app = create_app()
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=8000)
