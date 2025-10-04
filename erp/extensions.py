bcb47bc8cfccca1d5b3e5bb5b29121be69fc1cab
getson7070 <getson.ind@gmail.com>
2025-10-04 13:34:10 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -2,7 +2,6 @@ from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# Export always-present objects; we may initialize them conditionally in app.py
 socketio = SocketIO(cors_allowed_origins="*")
 db = SQLAlchemy()
 migrate = Migrate()
