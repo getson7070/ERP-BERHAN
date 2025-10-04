49efc08cacc6379d56ea756e67d4c07a5d48db68
getson7070 <getson.ind@gmail.com>
2025-10-04 15:02:56 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,10 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_socketio import SocketIO
+from flask_wtf import CSRFProtect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask_socketio import SocketIO
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
-limiter = Limiter(key_func=get_remote_address)  # storage set via app.config
-socketio = SocketIO(async_mode="eventlet", cors_allowed_origins="*")
+socketio = SocketIO(async_mode="eventlet")
+csrf = CSRFProtect()
+limiter = Limiter(key_func=get_remote_address, default_limits=["200/min"])
+login_manager = LoginManager()
