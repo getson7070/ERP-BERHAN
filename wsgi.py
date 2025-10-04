67ebfc6050c7a9726c2b86ddb4f4370c863dbc32
getson7070 <getson.ind@gmail.com>
2025-10-04 16:33:23 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
-# wsgi.py
 import eventlet
-eventlet.monkey_patch()  # must be first
+eventlet.monkey_patch()
 
-from erp.app import create_app
-
+from erp.app import create_app  # import AFTER monkey_patch
 app = create_app()
+
+if __name__ == "__main__":
+    app.run()
