075b39c2a3cafb328f835fd541d068d65fbe5e71
getson7070 <getson.ind@gmail.com>
2025-10-04 16:11:50 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,7 @@
 # wsgi.py
-import os
 import eventlet
-eventlet.monkey_patch()  # must happen before anything else
+eventlet.monkey_patch()  # must be first
 
 from erp.app import create_app
 
 app = create_app()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "10000")))
