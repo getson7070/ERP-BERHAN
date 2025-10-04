584da565f3ecc9f8dd6c76a0934e0bdd2d23fbcb
getson7070 <getson.ind@gmail.com>
2025-10-04 13:58:16 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,7 +1,11 @@
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 socketio = SocketIO(cors_allowed_origins="*")
 db = SQLAlchemy()
 migrate = Migrate()
+# Default limits can be tuned later; key_func uses client IP
+limiter = Limiter(key_func=get_remote_address)
