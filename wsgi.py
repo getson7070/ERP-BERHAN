6b104c286297a3414acb83a7f4f280a567bc3ee8
getson7070 <getson.ind@gmail.com>
2025-10-04 15:19:07 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
-# --- critical: eventlet must patch BEFORE any other import ---
+# wsgi.py
 import eventlet
 eventlet.monkey_patch()
 
-from erp.app import create_app
+from erp.app import create_app  # noqa: E402
 
-# Gunicorn will import this
 app = create_app()
+
+if __name__ == "__main__":
+    from erp.extensions import socketio  # noqa: E402
+    socketio.run(app, host="0.0.0.0", port=10000)
