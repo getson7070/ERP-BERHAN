4813954340f499850af20cab7e19343ab4d984df
getson7070 <getson.ind@gmail.com>
2025-10-04 14:57:09 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,10 +1,10 @@
-from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_socketio import SocketIO
 
-socketio = SocketIO(cors_allowed_origins="*")
 db = SQLAlchemy()
 migrate = Migrate()
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address)  # storage set via app.config
+socketio = SocketIO(async_mode="eventlet", cors_allowed_origins="*")
