150d95a9ea6ef93d069bc86ccee82694fbbfab02
getson7070 <getson.ind@gmail.com>
2025-10-04 13:02:52 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,16 +1,5 @@
-"""Application-wide extension instances (singletons)."""
-
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-from flask_mail import Mail
+# erp/extensions.py  (full replacement)
 from flask_socketio import SocketIO
 
-db = SQLAlchemy()
-migrate = Migrate()
-csrf = CSRFProtect()
-limiter = Limiter(key_func=get_remote_address, storage_uri="memory://")
-mail = Mail()
-socketio = SocketIO(async_mode="eventlet", logger=False, engineio_logger=False)
+# Do not hardcode async_mode here; we set it during init to avoid bad env overrides.
+socketio = SocketIO(cors_allowed_origins="*")
