5ed1f9ef70dc7245caf7fc36e9c717ea320dd9d7
getson7070 <getson.ind@gmail.com>
2025-10-06 21:58:11 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
+# wsgi.py
 import eventlet
-eventlet.monkey_patch()  # must be first
+eventlet.monkey_patch()
 
-import os
 from erp.app import create_app
 from erp.extensions import socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "5000"))
+    import os
+    port = int(os.getenv("PORT", 5000))
     socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
-
