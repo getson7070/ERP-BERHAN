aa542383270cc27caa61ba7e6384dbec10f7e726
getson7070 <getson.ind@gmail.com>
2025-10-07 18:02:46 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,7 @@
 # gunicorn.conf.py
-import os
-bind = f"0.0.0.0:{os.getenv('PORT', '10000')}"
-workers = int(os.getenv("WEB_CONCURRENCY", "1"))
+bind = "0.0.0.0:10000"
+workers = 1
 worker_class = "eventlet"
-graceful_timeout = int(os.getenv("GUNICORN_GRACEFUL_TIMEOUT", "120"))
-timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
-loglevel = os.getenv("GUNICORN_LOGLEVEL", "info")
+timeout = 120
+loglevel = "info"
+preload_app = True  # loads app once in master, then forks (helps with patching)
