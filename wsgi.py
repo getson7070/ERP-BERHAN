901c6fe95ccb88048586609a7aae35f6d62cf9e3
getson7070 <getson.ind@gmail.com>
2025-10-07 20:14:22 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
 # wsgi.py
 import eventlet
-eventlet.monkey_patch()  # MUST happen before any other imports when using eventlet workers
+eventlet.monkey_patch()  # must be first
 
 from erp.app import create_app
 
-# Gunicorn runs "wsgi:app" so we must expose a module-level "app"
 app = create_app()
 
 if __name__ == "__main__":
