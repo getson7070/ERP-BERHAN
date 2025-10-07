81322f6be77f377059b97ba72da2d1d53b2ad476
getson7070 <getson.ind@gmail.com>
2025-10-07 13:27:12 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,8 @@
+# IMPORTANT: do this before any other imports when using the eventlet worker
 import eventlet
-eventlet.monkey_patch()  # MUST run before any other imports that use networking/threads
+eventlet.monkey_patch()
 
 from erp.app import create_app
-from erp.extensions import socketio
 
+# Gunicorn command expects this name: "wsgi:app"
 app = create_app()
-
-# For local dev: `python wsgi.py`
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", "10000"))
-    socketio.run(app, host="0.0.0.0", port=port)
