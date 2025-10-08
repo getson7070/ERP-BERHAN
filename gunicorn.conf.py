a224f84cb05ccf1fcbb1175eb0dfda75c0d44a95
getson7070 <getson.ind@gmail.com>
2025-10-08 12:56:45 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,6 @@
 # gunicorn.conf.py
-from eventlet import monkey_patch
-monkey_patch()  # ensures green thread monkey patching before workers import anything
-
-bind = "0.0.0.0:10000"
-worker_class = "eventlet"
 workers = 1
-preload_app = True  # import app in master so monkey_patch happens early enough
+worker_class = "eventlet"
+bind = "0.0.0.0:10000"
+preload_app = False
 timeout = 120
-loglevel = "info"
-accesslog = "-"
-errorlog = "-"
