a87cf30e64fc82fb6193f77b8819f377e778bcfa
getson7070 <getson.ind@gmail.com>
2025-10-08 09:35:18 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,7 @@
 # wsgi.py
-import eventlet
-eventlet.monkey_patch()  # must be first
-
 from erp.app import create_app
 
+# Gunicorn looks for "wsgi:app"
 app = create_app()
 
 if __name__ == "__main__":
