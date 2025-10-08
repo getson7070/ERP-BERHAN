f85c1e242cdb6975ce2ed60337554a10bfb88005
getson7070 <getson.ind@gmail.com>
2025-10-08 12:50:43 +0300

    Update main.py

--- erp/routes/main.py
+++ erp/routes/main.py
@@ -1,14 +1,8 @@
 # erp/routes/main.py
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
 
 bp = Blueprint("main", __name__)
 
 @bp.get("/choose_login")
 def choose_login():
     return render_template("choose_login.html")
-
-@bp.get("/dashboard")
-@login_required
-def dashboard():
-    return render_template("dashboard.html", user=current_user)
