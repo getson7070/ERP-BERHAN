d73d0a62ff4ce093ddb799310f7ba2d3e90555a6
getson7070 <getson.ind@gmail.com>
2025-10-10 13:20:51 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,14 +1,10 @@
+# erp/extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
+from flask_wtf import CSRFProtect
 from flask_login import LoginManager
-from flask_caching import Cache
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
 login_manager = LoginManager()
-cache = Cache(config={"CACHE_TYPE": "SimpleCache"})  # OK for now
-limiter = Limiter(key_func=get_remote_address, storage_uri="memory://")
