e6b63e9ca3470824635887986bc3bb4be50eef06
getson7070 <getson.ind@gmail.com>
2025-10-11 00:26:11 +0300

    Create user.py

---
+++ erp/models/user.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from flask_login import UserMixin
+from erp.extensions import db
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
