c180444d6f86c02b193fd173dce4409fbff1d3ab
getson7070 <getson.ind@gmail.com>
2025-10-11 00:26:59 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,9 @@
-workers = 1
-worker_class = "eventlet"
+import os
+
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
+workers = 1                # Socket.IO + eventlet prefers 1 worker
+worker_class = "eventlet"  # match your Socket.IO async mode
 threads = 1
-preload_app = False
-bind = "0.0.0.0:10000"
 timeout = 120
 graceful_timeout = 120
-loglevel = "info"
+preload_app = False        # with eventlet, avoid preloading unless you know it's safe
