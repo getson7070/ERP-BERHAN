b7162bca2428c6ab49cdbd15f0e25f1f2bbd7e95
getson7070 <getson.ind@gmail.com>
2025-10-10 10:44:43 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,4 +1,10 @@
 # erp/extensions.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
+from flask_login import LoginManager
 
+db = SQLAlchemy()
+migrate = Migrate()
 csrf = CSRFProtect()
+login_manager = LoginManager()
