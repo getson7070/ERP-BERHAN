ba876a7e85a6613fb2ba37dca1e0af328c7413db
getson7070 <getson.ind@gmail.com>
2025-10-11 00:25:03 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -1,19 +1,8 @@
-# erp/extensions.py
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_cors import CORS
-from flask_caching import Cache
-from flask_mail import Mail
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
 db = SQLAlchemy()
-migrate = Migrate()
-login_manager = LoginManager()
-cors = CORS()
-cache = Cache()
-mail = Mail()
 
-# For Flask-Limiter 3.x, create Limiter with key_func; configure storage via app.config
-limiter = Limiter(key_func=get_remote_address)
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"         # adjust to your login route
+login_manager.login_message_category = "warning"
