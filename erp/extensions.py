971cede397f10165808e5c11253552f1ebf9ecd1
getson7070 <getson.ind@gmail.com>
2025-10-10 10:59:13 +0300

    Update extensions.py

--- erp/extensions.py
+++ erp/extensions.py
@@ -2,9 +2,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
-from flask_login import LoginManager
+from flask_login import LoginManager, AnonymousUserMixin
 
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
 login_manager = LoginManager()
+
+# Anonymous user with a default role
+class Anonymous(AnonymousUserMixin):
+    role = "anonymous"
+
+login_manager.anonymous_user = Anonymous
