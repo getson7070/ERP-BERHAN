58145a5713a90a1e262d367121e711860bc68edb
getson7070 <getson.ind@gmail.com>
2025-10-12 11:46:19 +0300

    Create 20251010_seed_test_users_and_device_placeholder.py

---
+++ migrations/versions/20251010_seed_test_users_and_device_placeholder.py
@@ -0,0 +1,11 @@
+# c20251010_seed_test_users_and_device_placeholder.py
+from alembic import op
+import sqlalchemy as sa
+
+revision = "cf161230ed7f"
+down_revision = "i1j2k3l4m5n"  # set to the *actual* parent in your history
+branch_labels = None
+depends_on = None
+
+def upgrade(): pass
+def downgrade(): pass
