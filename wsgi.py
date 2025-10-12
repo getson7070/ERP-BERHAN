318f31cd617e86763d95086e79edb83a08ffd574
getson7070 <getson.ind@gmail.com>
2025-10-12 21:19:24 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-# >>> add these two lines at the very top <<<
+# patch Eventlet BEFORE anything else is imported
 import eventlet
 eventlet.monkey_patch()
 
@@ -8,8 +8,4 @@ from erp import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # Never hardcode port on Render; use $PORT
-    port = int(os.environ.get("PORT", 8000))
-    # Use a simple dev server only when running locally
-    from werkzeug.serving import run_simple
-    run_simple("0.0.0.0", port, app, use_reloader=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
