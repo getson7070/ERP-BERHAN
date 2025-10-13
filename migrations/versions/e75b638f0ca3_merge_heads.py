1d68deb9af2eeac8905d468be8f859034766874a
getson7070 <getson.ind@gmail.com>
2025-10-14 01:08:07 +0300

    Update e75b638f0ca3_merge_heads.py

--- migrations/versions/e75b638f0ca3_merge_heads.py
+++ migrations/versions/e75b638f0ca3_merge_heads.py
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e75b638f0ca3'
-down_revision = ('000c349c7249', 'a0e29d7d0f58')
+revision = "e75b638f0ca3"
+down_revision = ("revL", "revM")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
