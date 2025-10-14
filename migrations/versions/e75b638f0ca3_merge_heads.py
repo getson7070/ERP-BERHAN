9986a6a1b74fee765f49b9005829ef2ca0656b5d
getson7070 <getson.ind@gmail.com>
2025-10-14 00:51:25 -0700

    Add files via upload

--- migrations/versions/e75b638f0ca3_merge_heads.py
+++ migrations/versions/e75b638f0ca3_merge_heads.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "e75b638f0ca3"
-down_revision = ("revL", "revM")
+down_revision = ("000c349c7249", "a0e29d7d0f58")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
