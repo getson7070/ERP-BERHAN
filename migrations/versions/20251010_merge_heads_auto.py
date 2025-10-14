8d7b7c64ed5f0bac195ac0121e03ba3927df4c10
getson7070 <getson.ind@gmail.com>
2025-10-14 09:46:07 -0700

    Add files via upload

--- migrations/versions/20251010_merge_heads_auto.py
+++ migrations/versions/20251010_merge_heads_auto.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "20251010_merge_heads_auto"
-down_revision = ("20241007_0001", "20250913_add_fk_indexes", "20251003_fix_kpi_sales_mv", "20251010_mfa_fields", "4879f87e41ba", "8d0e1f2g3h4i", "ae590e676162", "i1j2k3l4m5n")   # tuple only
+down_revision = ("20241007_0001", "20250913_add_fk_indexes", "20251003_fix_kpi_sales_mv", "20251010_mfa_fields", "4879f87e41ba", "8d0e1f2g3h4i", "ae590e676162", "i1j2k3l4m5n")  # tuple only
 branch_labels = None
 depends_on = None
 
