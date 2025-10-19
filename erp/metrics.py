aae6f9e337a6f2e3bd069523720538b0bbf9f2d0
Your Name <you@example.com>
2025-10-19 12:14:15 +0300

    metrics: simple Counter/Gauge stubs with ._value.set/get() and .labels()

---
+++ erp/metrics.py
@@ -0,0 +1,18 @@
+﻿class _Value:
+    def __init__(self, v=0): self._v=v
+    def get(self): return self._v
+    def set(self, v): self._v=v
+
+class _Counter:
+    def __init__(self): self._value=_Value(0)
+    def inc(self, n=1): self._value.set(self._value.get()+n); return self
+    def labels(self, *_, **__): return self  # simple stub
+
+class _Gauge:
+    def __init__(self): self._value=_Value(0)
+    def set(self, v): self._value.set(v); return self
+    def labels(self, *_, **__): return self
+
+GRAPHQL_REJECTS = _Counter()
+RATE_LIMIT_REJECTIONS = _Counter()
+QUEUE_LAG = _Gauge()
