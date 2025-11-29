b70f5a437cfd10bd5adaa1ff69e978460c1b596b
getson7070 <getson.ind@gmail.com>
2025-11-29 08:58:05 +0300

    Register offline route and harden offline page test (#158)

--- tests/test_offline_page.py
+++ tests/test_offline_page.py
@@ -5,6 +5,8 @@ def test_offline_page_available():
     app = create_app()
     client = app.test_client()
     resp = client.get("/offline")
-    assert b"The application is offline" in resp.data
+    assert resp.status_code == 200
+    assert resp.mimetype == "text/html"
+    assert resp.get_data(as_text=True) == "<html><body>The application is offline</body></html>"
 
 
